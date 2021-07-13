7eff9d0e219c3baf4c72ce99c266b2291e52b050
osumi-kyopuro <osumiyuki1123@gmail.com>
2021-07-14 02:04:45 +0900

    time is added

--- time.cpp
+++ time.cpp
@@ -634,7 +634,26 @@ struct Clock{
 
 
 int main() {
-    
+    // 入力を受け取る
+    int hour, minute, second;
+    cin >> hour >> minute >> second;
+    int diff_second;
+    cin >> diff_second;
+
+    // Clock構造体のオブジェクトを宣言
+    Clock clock;
+
+    // set関数を呼び出して時刻を設定する
+    clock.set(hour, minute, second);
+
+    // 時刻を出力
+    cout << clock.to_str() << endl;
+
+    // 時計を進める(戻す)
+    clock.shift(diff_second);
+
+    // 変更後の時刻を出力
+    cout << clock.to_str() << endl;
     
 }
    
