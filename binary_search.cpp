257448451edf48009dd8c32dbd628d98262407c3
osumi-kyopuro <osumiyuki1123@gmail.com>
2021-09-08 01:11:51 +0900

    lower_bound is added

--- binary_search.cpp
+++ binary_search.cpp
@@ -2,11 +2,11 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+#define all(a) (a).begin(), a.end()
 int main(){
-    /*1が何番目(0-indexed)にあるか二分探索で求めるアルゴリズム*/
-    bool flag=false;
+    /*1が何番目(0-indexed)にあるか二分探索で求めるプログラム*/
     vector<int>card={5,3,2,1,4};
-    sort(card.begin(),card.end());//昇順ソートする
+    sort(all(card));//昇順ソートする
     int ok=0,ng=5;//区間を決める
     while(ng-ok>1){//区間が1になるまで
         int mid=(ok+ng)/2;//探索範囲の真ん中
--- lower_bound.cpp
+++ lower_bound.cpp
@@ -2,23 +2,13 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
+#define all(a) (a).begin(), a.end()
 int main() {
-	//vector<int>a(100000);
-	int n;
-	cin >> n;
-	vector<int>a(n);
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-		//cout << a[i] << endl;
-	int q, k, sum = 0;
-	cin >> q;
-	for (int i = 0; i < q; i++) {
-		cin>>k;
-		if (*lower_bound(a.begin(),a.end(), k) == k) {
-			sum++;
-		}
-	}
-	
-	cout << sum << endl;
+	/*1が何番目(0-indexed)にあるか二分探索で求めるプログラム*/
+	vector<int>card={5,3,2,1,4};
+	sort(all(card));
+	//lower_bound:指定された要素以上の値が現れる最初の位置のイテレータを取得する。
+	auto iter=lower_bound(all(card),1);
+	int index=iter-card.begin();
+	cout<<index<<endl;
 }
