c80443457b702fb78087269b38e60605938a95e2
osumi-kyopuro <osumiyuki1123@gmail.com>
2021-09-08 10:53:26 +0900

    binary_search is added

--- binary_search.cpp
+++ binary_search.cpp
@@ -4,18 +4,23 @@
 using namespace std;
 #define all(a) (a).begin(), a.end()
 int main(){
-    /*1が何番目(0-indexed)にあるか二分探索で求めるプログラム*/
+    /*1があるか二分探索で求めるプログラム*/
     vector<int>card={5,3,2,1,4};
     sort(all(card));//昇順ソートする
-    int ok=0,ng=5;//区間を決める
+    int ok=-1,ng=5;//区間を決める
     while(ng-ok>1){//区間が1になるまで
         int mid=(ok+ng)/2;//探索範囲の真ん中
-        if(card[mid]>3){//1より大きい場合
+        if(card[mid]>1){//1より大きい場合
             ng=mid;
         }
-        else{//1より小さい場合
+        else{//それ以外
             ok=mid;
         }
     }
-    cout<<ok<<endl;
+    if(card[ok]==1){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
 }
--- lower_bound.cpp
+++ lower_bound.cpp
@@ -4,11 +4,16 @@
 using namespace std;
 #define all(a) (a).begin(), a.end()
 int main() {
-	/*1が何番目(0-indexed)にあるか二分探索で求めるプログラム*/
-	vector<int>card={5,3,2,1,4};
+	/*1があるか二分探索で求めるプログラム*/
+	vector<int>card={0,0,0,0,0};
 	sort(all(card));
 	//lower_bound:指定された要素以上の値が現れる最初の位置のイテレータを取得する。
 	auto iter=lower_bound(all(card),1);
 	int index=iter-card.begin();
-	cout<<index<<endl;
+	if(card[index]==1){
+		cout<<"Yes"<<endl;
+	}
+	else{
+		cout<<"No"<<endl;
+	}
 }
